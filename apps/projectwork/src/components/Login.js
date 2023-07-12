3bedc54ada1970fff0f6670b7cb89f81345d0d5a
getsuki <37655180+getsuki@users.noreply.github.com>
2023-07-12 19:49:50 +0800

    Update and rename Table.module.css to Login.js

---
+++ apps/projectwork/src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (username === 'username' && password === 'password') {
+      onLogin();
+    } else {
+      console.log('Invalid credentials');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
