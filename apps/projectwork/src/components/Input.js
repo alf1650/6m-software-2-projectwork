9399cac68ca5f3e865f0a03630b33a32f197812c
Huixuan <83389316+Titian27@users.noreply.github.com>
2023-07-12 19:42:09 +0800

    creation of input.js

---
+++ apps/projectwork/src/components/Input.js
@@ -0,0 +1,9 @@
+import { useState } from "React";
+
+function Input() {
+  const [date, setDate] = useState(0);
+  const [destination, setDestination] = useState(0);
+
+  const handlerAddDate = (e) => {};
+  return <></>;
+}
