d9cbc0b5435c9f7dc78000b99a1546e10f40f62f
getsuki <37655180+getsuki@users.noreply.github.com>
2023-07-18 21:41:30 +0800

    Update App.js

--- apps/projectwork/App.js
+++ apps/projectwork/App.js
@@ -1,38 +1,93 @@
-import React, { useState } from 'react';
-import Login from './components/Login';
-import Input from './components/Input';
-import Results from './components/Results';
-import Favorites from './components/Favorites';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import NotFound from "./components/NotFound";
+import Favorites from "./components/Favorites";
+import Footer from "./components/Footer";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [countryCode, setCountryCode] = useState('');
-  const [year, setYear] = useState(''); const [holidays, setHolidays] = useState([]);
+  const [isLoggedin, setIsloggedin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [currency, setCurrency] = useState("SGD");
+  const [favs, setFavs] = useState("");
 
-
-  const handleLogin = () => {
-    setLoggedIn(true);
+  const updateUsername = (value) => {
+    setUsername(value);
   };
 
-  const handleSearch = (selectedCountryCode, selectedYear) => {
-    setCountryCode(selectedCountryCode);
-    setYear(selectedYear);
+  const updateCurrency = (value) => {
+    setCurrency(value);
   };
 
+  const updateFavs = (newlist) => {
+    setFavs((prev) => [...prev, newlist]);
+  };
+
+  const updateFavNotes = (id, input) => {
+    setFavs(
+      favs.map((fav) => (fav.id === id ? { ...fav, notes: input } : fav))
+    );
+    console.log(input);
+  };
+
+  const deleteFav = (newlist) => {
+    setFavs(newlist);
+  };
 
   return (
-    <div>
-      {!loggedIn ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <>
-          <Input onSearch={handleSearch} />
-          <Results countryCode={countryCode} year={year} />
-          <Favorites />
-        </>
-      )}
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout
+                isLoggedin={isLoggedin}
+                setIsloggedin={setIsloggedin}
+                username={username}
+                updateUsername={updateUsername}
+                currency={currency}
+                updateCurrency={updateCurrency}
+                favs={favs}
+              />
+            }
+          >
+            <Route
+              index
+              element={
+                <Login
+                  setIsloggedin={setIsloggedin}
+                  updateUsername={updateUsername}
+                  currency={currency}
+                  updateCurrency={updateCurrency}
+                />
+              }
+            />
+            <Route path="search" element={<Search username={username} />} />
+            <Route
+              path="results"
+              element={<Results currency={currency} updateFavs={updateFavs} />}
+            />
+            <Route
+              path="favorites"
+              element={
+                <Favorites
+                  favs={favs}
+                  updateFavNotes={updateFavNotes}
+                  deleteFav={deleteFav}
+                />
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
-};
-
+}
 export default App;
