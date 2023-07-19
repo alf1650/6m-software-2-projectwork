5f6f016de5544ec6d63d5a6dff94e84efb255713
getsuki <37655180+getsuki@users.noreply.github.com>
2023-07-19 21:53:11 +0800

    Update Login.js

--- apps/projectwork/src/components/Login.js
+++ apps/projectwork/src/components/Login.js
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';
+import styles from "./Login.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CurrencySelector from "./CurrencySelector";
 
-const Login = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+function Login({ setIsloggedin, updateUsername, currency, updateCurrency }) {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    if (username === 'username' && password === 'password') {
-      onLogin();
-    } else {
-      console.log('Invalid credentials');
-    }
+  const handleClick = () => {
+    updateUsername(input);
+    navigate("/search");
+    setIsloggedin(true);
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className={styles.login}>
+      <div className={styles.content}>
+      <h1>Welcome to TravelBud!</h1>
+      <br />
+      <p>Please enter your name to start using app</p>
+      <br />
+      <label>Name: </label>
+      <input
+        type="text"
+        placeholder="e.g. John"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <br />
+      <label>Select your currency: </label>
+      <CurrencySelector currency={currency} updateCurrency={updateCurrency} />
+      <br />
+      <br />
+      <button
+        className={styles.button}
+        disabled={input ? false : true}
+        onClick={handleClick}
+      >
+        Start Your Journey
+      </button>
+    </div>
     </div>
   );
-};
+}
 
 export default Login;
